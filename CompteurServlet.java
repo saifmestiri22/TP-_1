a9f5297a1f82d5cae148e4517ad4cc8ac12ca670
saifmestiri22 <101934243+saifmestiri22@users.noreply.github.com>
2022-05-18 17:17:33 +0100

    Add files via upload

---
+++ CompteurServlet.java
@@ -0,0 +1,49 @@
+package isetj.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class CompteurServlet
+ */
+@WebServlet("/CompteurServlet")
+public class CompteurServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public CompteurServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session= request.getSession();
+		Integer compteur =(Integer)session.getAttribute("compteur");
+		if(compteur==null)
+			compteur=0;
+		compteur++;
+		session.setAttribute("compteur", compteur);
+		PrintWriter out= response.getWriter();
+		out.println("vous avez visualisé cette page "+compteur+" fois depuis l'ouverture de session");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
